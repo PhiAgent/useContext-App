7ab6f913c74bdd50a1eab5e592990900b0966c56
RasNewton <princegyekyeaddai@gmail.com>
2021-11-02 04:54:39 -0500

    first

--- src/App.js
+++ src/App.js
@@ -11,3 +11,13 @@ function App() {
 }
 
 export default App;
+
+var reducer = (state, action) => {
+  switch (action.type) {
+    case 'both':
+      return {name: 'Paa', age: 29}
+    case 'any':
+      return {age: 27}
+  }
+};
+var [state, dispatch] = useReducer(reducer, {name: 'Prince', age: 28})
